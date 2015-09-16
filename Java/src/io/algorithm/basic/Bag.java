76bd85c0e716cf13407008e98f5e95446f345248
Hill1942 <ykdacd123@gmail.com>
2015-09-16 17:20:23 +0800

    bag

--- Java/src/io/algorithm/basic/Bag.java
+++ Java/src/io/algorithm/basic/Bag.java
@@ -1,8 +1,53 @@
 package io.algorithm.basic;
 
+import java.util.Iterator;
+
 /**
  * Created by Kaidi on 2015/9/7.
  */
-public class Bag {
+public class Bag<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+    private class Node {
+        Item item;
+        Node next;
+    }
 
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void add(Item item) {
+        Node oldfist = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfist;
+
+        N++;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+
+        }
+    }
 }
